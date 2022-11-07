020159e9b533973e1b2eb0731c3cf89e638d2bdc
AKACHI-4 <Adarshrawat8304@gmail.com>
2022-11-07 23:35:25 +0530

    Learned DOM tree and its traversals

--- DOM Tutorial/DOM_deep.js
+++ DOM Tutorial/DOM_deep.js
@@ -1,8 +1,8 @@
 // loop <-> Iterate Elements
 
 // get multiple elements using getElements by class name ⬇️
-const navItems = document.getElementsByTagName("a"); 
-console.log(navItems); // HTMLCollection(3) [a, a, a]
+// let navItems = document.getElementsByTagName("a"); 
+// console.log(navItems); // HTMLCollection(3) [a, a, a]
 
 // forEach loop can not be used with HTMLCollection we only can use simple for & for of loop here in terms of made change on all elements of collection 
  
@@ -22,6 +22,13 @@ console.log(navItems); // HTMLCollection(3) [a, a, a]
 // }
 
 // What if I have to change from HTMLCollection to Array 
+// navItems = Array.from(navItems);
+// console.log(Array.isArray(navItems)); // True
+// navItems.forEach((navItem) => {
+//     navItem.style.backgroundColor = "#fff"; 
+//     navItem.style.color = "blue";
+//     navItem.style.fontWeight = "bold";    
+// }); // changes can be seen into the webpage
 
 // for each loop
 // navItems.forEach((navItem) => {
@@ -32,5 +39,46 @@ console.log(navItems); // HTMLCollection(3) [a, a, a]
 
 // get multiple items using querySelectorAll ⬇️
 
-// const navItems = document.querySelectorAll(".nav-item")
-// console.log(navItems); 
+// let navItems = document.querySelectorAll("a");
+// console.log(navItems); // NodeList(3) [a, a, a]
+// here we get a NodeList 
+// In a NodeList we can use all iterative methods: Simple for loop, for each loop and for of loop
+
+// Simple for loop
+// for(let i=0;i<navItems.length;i++){
+//     const navItem = navItems[i]; 
+//     navItem.style.backgroundColor = "#fff"; 
+//     navItem.style.color = "blue";
+//     navItem.style.fontWeight = "bold";
+// } // will run perfectly 
+
+// for of loop
+// for(let navItem of navItems){
+//     navItem.style.backgroundColor = "#fff"; 
+//     navItem.style.color = "green";
+//     navItem.style.fontWeight = "bold";
+// } // will run perfectly 
+
+// for each loop
+// navItems.forEach((navItem) => {
+//     navItem.style.backgroundColor = "#fff"; 
+//     navItem.style.color = "red";
+//     navItem.style.fontWeight = "bold";    
+// }); // changes can be seen into the webpage
+
+// we also can change NodeList into an array using Array.from() as same as in HTMLCollection
+
+
+/* innerHTML */
+// HTML which is written inside a particular tag can be said as inner HTML of that tag
+const headLine = document.querySelector(".headline"); 
+// console.log(headLine); // <div class="headline">…</div>
+// ... part comes under inner HTML of headline
+
+console.log(headLine.innerHTML);
+/* <h2 id="main-heading">Manage your task<span style="display: none;"> hello</span></h2>
+<button class="btn btn-headline">Learn more</button> */
+
+headLine.innerHTML = "<h1>Inner HTML changed</h1>"
+headLine.innerHTML += "<button class=\"btn\">Learn More</button>"
+console.log(headLine.innerHTML); // <h1>Inner HTML changed</h1><button class="btn">Learn More</button>
